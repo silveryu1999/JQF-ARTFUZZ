bd1acb8eeeb6a009e301eb1a33538246da3360ec
silveryu1999 <931834214@qq.com>
2025-02-24 10:00:37 +0800

    update shells

--- fuzzing/compiler/run.sh
+++ fuzzing/compiler/run.sh
@@ -3,10 +3,14 @@
 #scripts=("run-zest.sh" "run-zest-slow-10240-2.sh" "run-zest-slow-10240-3.sh" "run-zest-slow-10240-4.sh" "run-zest-slow-10240-5.sh" "run-zest-slow-40960-1.sh" "run-zest-slow-40960-2.sh" "run-zest-slow-40960-3.sh" "run-zest-slow-40960-4.sh" "run-zest-slow-40960-5.sh" "run-art-slow-10240-1.sh" "run-art-slow-40960-1.sh")
 
 scripts=(
-"run-art.sh"
-"run-random.sh"
-"run-zest.sh"
-"run-rank.sh"
+# "run-art.sh"
+# "run-random.sh"
+# "run-zest.sh"
+"run-rank-1.sh"
+"run-rank-2.sh"
+"run-rank-3.sh"
+"run-rank-5.sh"
+"run-rank-10.sh"
 )
 
 for script in "${scripts[@]}"; do
